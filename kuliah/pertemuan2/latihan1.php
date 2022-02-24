67b4c401f36d02e13dfd146a06966883ab7698d2
Wina Agustina <wina.213040093@mail.unpas.ac.id>
2022-02-24 23:13:20 +0700

    Mengerjakan latihan di pertemuan 3

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,53 @@
+<?php
+// VARIABEL
+// tempat penyimpanan nilai
+// Nilai; angka (integer), tulisan (string), boolean (true/false)
+
+// Nama bebas, tidak boleh ada spasi
+// boleh ada angka, tapi tidak diawal
+$nama = "WinaAgustina";
+echo $nama;
+echo "<hr>";
+
+
+
+// OPERATOR
+// Aritmatika
+// +, -, *, / (kalibataku), % (modulo,modulus,sisa bagi)
+echo 10 * 10 + 2 - 1;
+echo "<br>";
+echo 2 % 2;
+echo "<hr>"
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+echo 1 < 2; //true / 1
+echo "<br>"
+echo 10 < 5; //false / null
+echo "<br>"
+echo 10 == 10;
+echo "<hr>"
+
+// Identitas
+// ===, !==
+// mengecek nilai dan tipe data
+echo 10 === "10";
+echo "<hr>"
+
+// Increment / Decrement
+// Penambahan / Pengurangan
+// ++, --
+$x = 10;
+
+echo $x++;
+echo --$x;
+echo "<br>";
+echo ++$x;
+
+// Concat, Penggabung String
+// .
+$nama_depan = "Wina";
+$nama_belakang ="Agustina"
+echo $nama_depan . $nama_belakang
+?>
+
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,17 @@
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// ternary
+
+$x = 20;
+if($x <20 ){
+    echo "benar";
+} elseif ($x == 20) {
+    echo "bingo!";
+}
+else{
+    echo "salah";
+}
+?>
